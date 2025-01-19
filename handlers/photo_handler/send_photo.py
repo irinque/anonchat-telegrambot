bf79cc690350259ea9bfb9a5f3b73ce5124cac9c
irinque <114353433+irinque@users.noreply.github.com>
2025-01-19 21:06:32 +0300

    v1.1

--- handlers/photo_handler/send_photo.py
+++ handlers/photo_handler/send_photo.py
@@ -1,19 +1,16 @@
 from aiogram import Bot
-from aiogram.types import Message
+from aiogram.types import Message, InputFile
 from settings.messages import message_registration, message_ban
 from database.db import db
 
-async def handler_private_chat(message: Message, bot: Bot):
+async def send_photo(message: Message, bot: Bot, photo_id):
     if db.user_exists(message.from_user.id):
         if db.user_check_ban(message.from_user.id):
             await bot.send_message(message.from_user.id, message_ban, parse_mode="Markdown")
         else:
             if db.check_session(message.from_user.id):
                 interlocutor = db.get_user_from_session(message.from_user.id)
-                db.add_user_message(message.from_user.id, message.text)
-                if message.reply_to_message:
-                    await bot.send_message(interlocutor, message.text, parse_mode="Markdown", reply_to_message_id=message.reply_to_message.message_id)
-                else:
-                    await bot.send_message(interlocutor, message.text, parse_mode="Markdown")
+                db.add_user_image(message.from_user.id, photo_id=photo_id)
+                await bot.send_photo(interlocutor, InputFile(f"img/users/{photo_id}.jpg"))
     else:
         await bot.send_message(message.from_user.id, message_registration, parse_mode="Markdown")
