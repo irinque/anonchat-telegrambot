9918a68d55f0397ffe3bcc17f9b05e1878e5909a
irinque <114353433+irinque@users.noreply.github.com>
2025-01-22 23:26:35 +0300

    v1.2
    clearing photo metadata

--- handlers/photo_handler/download.py
+++ handlers/photo_handler/download.py
@@ -1,5 +1,10 @@
 from aiogram.types import Message
+from exif import Image
 
 async def download(message: Message):
     await message.photo[-1].download(destination_file="img/users/" + message.photo[-1].file_unique_id + ".jpg")
+    with open("img/users/" + message.photo[-1].file_unique_id + ".jpg", "rb") as image:
+        image = Image()
+        if image.has_exif:
+            image.delete_all()      
     return message.photo[-1].file_unique_id
